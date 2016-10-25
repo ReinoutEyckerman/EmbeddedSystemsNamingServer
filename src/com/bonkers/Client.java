63a0412ee5386e120aff68551dcaacc29add4de6
KennyEmbrechts <kenny.embrechts@gmail.com>
2016-10-25 11:33:51 +0200

    ServerInterface en Client RMI template toegevoegd

--- src/com/bonkers/Client.java
+++ src/com/bonkers/Client.java
@@ -1,7 +1,27 @@
 package com.bonkers;
 
-/**
- * Created by Kenny on 25/10/16.
- */
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
+    public static void main(String args[]) throws Exception {
+        String host = "192.168.1.1";
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            ServerIntf stub = (ServerIntf) registry.lookup("ServerIntf");
+            int response = stub.getBalance();
+            System.out.println(response);
+            stub.withdrawMoney(10);
+            response = stub.getBalance();
+            System.out.println(response);
+            stub.addMoney(40);
+            response = stub.getBalance();
+            System.out.println(response);
+
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
 }
