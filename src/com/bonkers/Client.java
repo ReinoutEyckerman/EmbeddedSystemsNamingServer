8651706264f3f5adc21b316b04cdcd363234ca90
Jente <jente.adams@gmail.com>
2016-11-03 17:01:19 +0100

    Client can send nodename

--- src/com/bonkers/Client.java
+++ src/com/bonkers/Client.java
@@ -1,6 +1,12 @@
 package com.bonkers;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -13,6 +19,11 @@ import java.util.List;
 import com.bonkers.ServerIntf;
 
 public class Client {
+    String ServerAddress = "192.168.1.1";
+    BufferedReader br = null;
+    DatagramSocket socket = null;
+    DatagramPacket packet = null;
+
     public static void main(String args[]) throws Exception {
         String host = "192.168.1.1";
         try {
@@ -47,5 +58,46 @@ public class Client {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+    }
+    private void sendDetailsToNameServer() throws IOException
+    {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // get a datagram socket
+        socket = new DatagramSocket();
+
+        sendRequest();
+
+        // display response
+        String received = new String(packet.getData(), 0, packet.getLength());
+        if(received == "Name is already taken")
+        {
+            System.out.println("The node name already exists on the server please choose another one");
+            sendRequest();
+        }
+        else if (received == "IP already exists")
+        {
+            System.out.println("You already exist in the name server");
+        }
+        else
+        {
+            System.out.println("No errors");
+        }
+
+        socket.close();
+    }
+    private void sendRequest() throws IOException
+    {
+        // send request
+        System.out.print("Give your client a name");
+        String name = br.readLine();
+        byte[] buf = new byte[2048];
+        buf = name.getBytes();
+        InetAddress address = InetAddress.getByAddress(ServerAddress.getBytes());
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6790);
+        socket.send(packet);
+
+        // get response
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
     }
 }
