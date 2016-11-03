5a4504a0f7df1d06aea9281b1d97f76a5697d240
Jente <jente.adams@gmail.com>
2016-11-03 14:11:40 +0100

    Fixed JSON errors

--- src/com/bonkers/HashTableCreator.java
+++ src/com/bonkers/HashTableCreator.java
@@ -1,7 +1,9 @@
 package com.bonkers;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JsonObject;
 
+import java.io.FileWriter;
 import java.util.Hashtable;
 
 /**
@@ -20,16 +22,16 @@ public class HashTableCreator {
     {
         int digest = createHash(ip);
         htName.put(name, digest);
-        htIp.put(digest, ip;
-        JSONObject joHtIp = new JSONObject(htIp);
+        htIp.put(digest, ip);
+        JsonObject joHtIp = new JsonObject(htIp);
         try{
-            FileWriter fw = new FileWriter("hashtable.json")
-            fw.write(joHtIp.toJSONString());
+            FileWriter fw = new FileWriter("hashtable.json");
+            fw.write(joHtIp.toJson());
             fw.close();
         }
         catch (Exception e)
         {
-            System.out.Println(e);
+            System.out.println(e);
         }
     }
 }
