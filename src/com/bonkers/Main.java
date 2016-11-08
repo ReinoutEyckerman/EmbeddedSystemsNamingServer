64a0d9ba02902aba1a9e305ad1972d3c44a1da04
ReinoutEyckerman <reinouteyckerman@gmail.com>
2016-11-08 09:23:12 +0100

    Fixed null checker in main

--- src/com/bonkers/Main.java
+++ src/com/bonkers/Main.java
@@ -7,10 +7,11 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        switch (args[0]) {
-            case "server":
-                System.out.println("Starting Server");
-                Server server = new Server();
+        if(args!=null) {
+            switch (args[0]) {
+                case "server":
+                    System.out.println("Starting Server");
+                    Server server = new Server();
                 /* try{
                     //server.run();
                 } catch (IOException e){
@@ -18,24 +19,31 @@ public class Main {
                     System.exit(1);
                 }*/
 
-                break;
-            case "client":
-                if (IsIP(args[1])) {
-                    Client client = new Client();
+                    break;
+                case "client":
+                    if (IsIP(args[1])) {
+                        Client client = new Client();
                     /*try {
                         client.run();
                     } catch (IOException e) {
                         System.out.println("Client "+e+"ge IOException caught. Exiting...");
                         System.exit(1);
                     }*/
-                } else {
-                    System.out.println("Argument is not a valid IPv4 address");
-                }
-                break;
-            default:
-                System.out.println("Unknown parameter: " + args[0]);
-                System.out.println("Exiting...");
+                    } else {
+                        System.out.println("Argument is not a valid IPv4 address");
+                    }
+                    break;
+                default:
+                    System.out.println("Unknown parameter: " + args[0]);
+                    System.out.println("Exiting...");
+            }
         }
+        else
+        {
+            System.out.println("Please enter a parameter");
+            System.out.println("Exiting...");
+        }
+
     }
 
     public static boolean IsIP(String text) {
