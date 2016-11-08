aae245be7ce2bd8d61bbcf557345da7c325e5396
Jente <jente.adams@gmail.com>
2016-11-08 12:11:34 +0100

    added NameServer hashtabelcraetor to new thread

--- src/com/bonkers/Server.java
+++ src/com/bonkers/Server.java
@@ -5,15 +5,9 @@ import java.net.*;
 
 public class Server implements ServerIntf {
 
-    private DatagramSocket socket = null;
-    private HashTableCreator HT = null;
-    private String Nodename = null;
-    private Boolean IsFinished = false;
-
     public Server() throws IOException {
-        HT = new HashTableCreator();
-        socket = new DatagramSocket(6790);
-        GetIP();
+        GetIPThread CreateHashTable = new GetIPThread();
+        CreateHashTable.start();
     }
 
     public String FindLocationFile(String FileName){
@@ -25,58 +19,6 @@ public class Server implements ServerIntf {
             return result;
         else
             return " File Not Found";
-    }
-
-    private void GetIP()
-    {
-        try {
-            byte[] buf = new byte[2048];
-
-            while (!IsFinished)
-            {
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                socket.receive(packet);
-                System.out.println("packet received" + packet.getData());
-                InetAddress address = packet.getAddress();
-                //InetAddress address = InetAddress.getByName(Address);
-                System.out.println(address);
-                int port = packet.getPort();
-                Nodename = new String(packet.getData());
-                System.out.println(Nodename);
-                String resp = checkDoubles(Nodename, address);
-                System.out.println(resp);
-                buf = resp.getBytes();
-                packet = new DatagramPacket(buf, buf.length, address, port);
-                DatagramSocket socket = new DatagramSocket();
-                socket.send(packet);
-                System.out.println(new String(packet.getData()));
-            }
-        }
-        catch (IOException e)
-        {
-            IsFinished = true;
-            System.out.println(e);
-        }
-    }
-
-    private String checkDoubles(String name, InetAddress ip)
-    {
-        String resp = null;
-        int hash = HT.createHash(name);
-        if(HT.htIp.containsKey(hash))
-        {
-            resp = "Name is already taken";
-        }
-        else if (HT.htIp.containsValue(ip))
-        {
-            resp = "IP already exists";
-        }
-        else
-        {
-            resp = "OK";
-            HT.CreateHashTable(ip, Nodename);
-        }
-        return resp;
     }
 }
 
