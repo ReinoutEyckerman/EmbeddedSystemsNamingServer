f52ef1750bd88d7acf2264d1053ad1aca6f6a834
ReinoutEyckerman <reinouteyckerman@gmail.com>
2016-11-09 11:46:42 +0100

    Server Shutdown/Failure RMI

--- src/com/bonkers/RMIServer.java
+++ src/com/bonkers/RMIServer.java
@@ -3,13 +3,14 @@ package com.bonkers;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 /**
  * Created by Jente on 8/11/2016.
  */
 public class RMIServer implements Callable, ServerIntf {
-
+    Map hashmap=new HashMap();
     public String FindLocationFile(String FileName){
         HashTableCreator obj = new HashTableCreator();
         String FileHash = FileName;//obj.createHash(FileName);
@@ -23,13 +24,21 @@ public class RMIServer implements Callable, ServerIntf {
 
     @Override
     public void NodeShutdown(Tuple node) {
-        //TODO: WHAT THE FUCK?
+        //TODO: WHOAT THE FUCK?
+        if(hashmap.containsKey(node.x)){
+            hashmap.remove(node.x);
+        }
+        else throw new IllegalArgumentException("Somehow, the node that shut down didn't exist");
     }
 
     @Override
     public Tuple<Tuple<Integer, String>, Tuple<Integer, String>> NodeFailure(Tuple node) {
-        //TODO: Zie NodeShutdown
-        return null;
+        List list=new ArrayList(hashmap.keySet());
+        Collections.sort(list);
+        int index=list.indexOf(node.x);
+        Tuple<Integer, String> previousNeighbor=new Tuple<Integer,String>((Integer)list.get(index-1),(String)hashmap.get(list.get(index-1)));
+        Tuple<Integer, String> nextNeighbor=new Tuple<Integer,String>((Integer)list.get(index+1),(String)hashmap.get(list.get(index+1)));
+        return new Tuple<>(previousNeighbor,nextNeighbor);
     }
 
     public RMIServer() {}
